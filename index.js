c7d30beb916c8130a0e5a7f8a8ab2d1e8a5f9b17
Andrew Zajac <ajzajac@icloud.com>
2019-06-20 23:04:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function deleteFromObjectByKey(object, key){
+  var newObj
   delete object[key]
   return object
 }
