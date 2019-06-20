f2da5eddb7bd46f1087c75ab7d77b5a96f19fecf
Andrew Zajac <ajzajac@icloud.com>
2019-06-20 22:51:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {cake: "flour"}
 
-function updateObjectWithKeyAndValue(recipes, key, value){
+function updateObjectWithKeyAndValue(recipes, cake, eggs){
   recipes[cake] = eggs
   return recipes
 }
