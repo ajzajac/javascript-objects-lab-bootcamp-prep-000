f7696fed4760c6c6a4ba439576ed8bb2dea53e03
Andrew Zajac <ajzajac@icloud.com>
2019-06-20 23:06:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,4 +12,7 @@ function deleteFromObjectByKey(object, key){
   delete newObj[key]
   return newObj
 }
-function destructivelyDeleteObjectByKey()
+function destructivelyDeleteObjectByKey(object, key){
+  delete object[key]
+  return object
+}
